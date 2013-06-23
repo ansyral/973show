8048d6316698372ac9ff1714a8eb084563e8c6d5
ZHOU Xuan <xuanzhou2011@163.com>
2013-06-23 14:59:38 +0800

    add the legend of the streamgraph

--- streamgraph.php
+++ streamgraph.php
@@ -8,14 +8,32 @@ body {
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
   margin: auto;
   position: relative;
-  width: 960px;
+  width: 100%;
+  height:100%
 }
 
-button {
-  position: absolute;
-  right: 10px;
-  top: 10px;
+#stream{
+	float:left;
+	width:80%;
+	margin:5px,5px,5px,5px;
+	
+	
 }
+#verticalline{
+	float:left;
+	width: 0.1%; 
+	height: 600px; 
+	background-color: #222222
+}
+#legend{
+	float:left;
+	width:19.9%;
+	margin:5px,5px,5px,5px;
+}
+
+#descrip{
+	margin-left: 40px;
+}　   
 
 </style>
 
@@ -23,10 +41,30 @@ button {
 <script src="http://yui.yahooapis.com/3.10.1/build/yui/yui-min.js"></script>
 </head>
 <body>
-<script>
-YUI().use('node','io-base','json-parse',function(Y){
-	Y.io('geteventheat.php',{
-		data:{'topicid':1},
+<div id="stream">
+	<div>
+		<table style="width:100%">
+		<tr>
+			<td>Jan</td>
+			<td >Feb</td>
+			<td >Mar</td>
+			<td >Apr</td>
+			<td >May</td>
+			<td >Jun</td>
+			<td >Jul</td>
+			<td >Aug</td>
+			<td >Sep</td>
+			<td >Oct</td>
+			<td >Nov</td>
+			<td >Dec</td>
+		</tr>
+		</table>
+	</div>
+	<div >
+		<script>
+		YUI().use('node','io-base','json-parse',function(Y){
+		Y.io('geteventheat.php',{
+		data:{'topicid':30},
 		on:{
 			complete:function(id,response){
 				if(response.status>=200&&response.status<300){
@@ -37,6 +75,7 @@ YUI().use('node','io-base','json-parse',function(Y){
 					}
 					else
 					{
+						
 						var n = datas.num, // number of layers
     						m = 365, // number of samples per layer
     						stack = d3.layout.stack().offset("wiggle")
@@ -60,13 +99,18 @@ YUI().use('node','io-base','json-parse',function(Y){
 
 						var color = d3.scale.linear()
   									  .range([ "#CD5C5C","#FF9900"]);
-
+						/*alert(color(Math.sin(0)));
+						alert(color(Math.sin(20)));
+						alert(color(Math.sin(40)));
+						alert(color(Math.sin(60)));
+						alert(color(Math.sin(80)));
+						alert(color(Math.sin(100)));*/
 						var area = d3.svg.area()
    									 .x(function(d) { return x(d.x); })
    									 .y0(function(d) { return y(d.y0); })
  									 .y1(function(d) { return y(d.y0 + d.y); });
 
-						var svg = d3.select("body").append("svg")
+						var svg = d3.select("div#stream div").append("svg")
   								    .attr("width", width)
   								    .attr("height", height);
 
@@ -90,48 +134,62 @@ YUI().use('node','io-base','json-parse',function(Y){
 
 						// Inspired by Lee Byron's test data generator.
 						
+							}
+						}
 					}
 				}
-			}
+			});
+		});
+
+		function sortEvent(a,b)
+		{
+			return a.heat - b.heat;
 		}
-	});
-});
+		function bumpLayer(n,basicv) {
 
-function sortEvent(a,b)
-{
-	return a.heat - b.heat;
-}
-function bumpLayer(n,basicv) {
+			function bump(a) {
+  				var x = 1 / (.1 + Math.random()),
+       			y = 2 * Math.random() - .5,
+      			z = 10 / (.1 + Math.random());
+   				for (var i = 0; i < n; i++) {
+     				var w = (i / n - y) * z;
+     				a[i] += x * Math.exp(-w * w);
+   				}
+  			}
 
-	function bump(a) {
-  		var x = 1 / (.1 + Math.random()),
-       		y = 2 * Math.random() - .5,
-      		z = 10 / (.1 + Math.random());
-   		for (var i = 0; i < n; i++) {
-     		var w = (i / n - y) * z;
-     		a[i] += x * Math.exp(-w * w);
-   		}
-  	}
+  			var a = [], i;
+ 			var cushion=10,delta;
+  			for (i = 0; i < n; ++i) 
+  			{
+  				if(basicv.started_at-cushion<=i&&i<=basicv.ended_at+cushion)
+  				{
+  					if(basicv.started_at<=i&&i<=basicv.ended_at)
+  						delta=0;
+  					else if(basicv.started_at>i)
+  						delta=basicv.started_at-i;
+  					else
+  						delta=i-basicv.ended_at;
+  					a[i] = basicv.heat*Math.exp(-0.01*delta*delta);
+  				}
+  				else 
+  					a[i]=0;
+  			}
+  			for (i = 0; i < 5; ++i) bump(a);
+			return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
+		}
+		</script>
+	</div>
 
-  	var a = [], i;
- 	var cushion=10,delta;
-  	for (i = 0; i < n; ++i) 
-  	{
-  		if(basicv.started_at-cushion<=i&&i<=basicv.ended_at+cushion)
-  		{
-  			if(basicv.started_at<=i&&i<=basicv.ended_at)
-  				delta=0;
-  			else if(basicv.started_at>i)
-  				delta=basicv.started_at-i;
-  			else
-  				delta=i-basicv.ended_at;
-  			a[i] = basicv.heat*Math.exp(-0.01*delta*delta);
-  		}
-  		else 
-  			a[i]=0;
-  	}
-  	for (i = 0; i < 5; ++i) bump(a);
-	return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
-}
-</script>
+</div>
+<div id="verticalline">
+</div>
+<div id="legend">
+	<div id="descrip">
+		Each shape shows how is one event's heat.
+	</div>
+	<div>
+		<img src="./img/legend1.jpg"/>
+	</div>
+	
+</div>
 </body>
